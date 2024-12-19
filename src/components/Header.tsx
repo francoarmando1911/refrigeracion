3c01ca788c59ab2288b622f4e261a6d92c9f4270
Franco Armando <francoarmando1911@gmail.com>
2024-12-19 20:35:13 -0300

    inicializacion del header

---
+++ src/components/Header.tsx
@@ -0,0 +1,11 @@
+
+export default function Header() {
+    return (
+        <>
+            <header className="relative overflow-hidden">
+                <div className="bg-gradient-to-r from-[#000B54] to-purple-900 h-[500px]">
+                </div>
+            </header>
+        </>
+    )
+}
